6ea3da19425858392b9919ec316f7f462eb62d12
Carson Lau <calau@ra.rockwell.com>
2022-10-27 15:07:59 -0400

    Create reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let stringArray = inputString.split('')
+    stringArray.reverse();
+    
+    return stringArray.join('');
 };
 
 // Do not edit below this line
