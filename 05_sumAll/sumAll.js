0e22f0796999565dd9121b69b704041ecaba37d8
Carson Lau <carselau@gmail.com>
2022-10-28 14:40:40 -0400

    Write sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  let start = 0;
+  let end = 0;
 
+  if (typeof a === "number" && typeof b === "number") {
+    if (a < b && a > 0) {
+      start = a;
+      end = b;
+    } else if (b < a && b > 0) {
+      start = b;
+      end = a;
+    } else {
+        return "ERROR"
+    }
+  } else {
+    return "ERROR";
+  }
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
