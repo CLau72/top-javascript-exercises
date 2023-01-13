f9ac4f8c47dbb86294bde2dd3c91afafa2d524ed
Carson Lau <calau@ra.rockwell.com>
2023-01-13 09:02:35 -0500

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (input) {
 
+const noPunctuation = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ").join("").toLowerCase();
+const reversedString = noPunctuation.split('').reverse().join('')
+
+if (reversedString === noPunctuation) {
+    return true;
+} else {
+    return false;
+}
+
+
+return noPunctuation;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
