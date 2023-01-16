68e82ba335278805732d64d656dcb7cc1e233e15
Carson Lau <carselau@gmail.com>
2023-01-15 21:48:51 -0500

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (value) {
+  const fibNumber = parseInt(value);
+  let fibArray = [0, 1];
 
+  if (fibNumber < 0) {
+    return "OOPS"
+  }
+
+  for (let i = 1; i < fibNumber; i++) {
+    nextNumber = fibArray.slice(-1)[0] + fibArray.slice(-2)[0]
+    fibArray.push(nextNumber)
+  }
+  return fibArray.slice(-1)[0];
 };
 
 // Do not edit below this line
