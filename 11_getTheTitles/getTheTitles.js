de00b20d56e8ccd8b42d94560525857fd7d5c9d3
Carson Lau <carselau@gmail.com>
2023-01-18 09:36:59 -0500

    Complete get the title exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArray) {
+    let bookTitles = [];
+    for (const book in bookArray) {
+        bookTitles.push(bookArray[book].title)
+    }
+    return bookTitles;
 };
 
 // Do not edit below this line
